5c2effae7bf0cb0e545e729a2e589482dcc840cf
GarlicBuns19 <planterx26@gmail.com>
2022-06-20 14:30:39 +0200

    help

--- todoApp.js
+++ todoApp.js
@@ -1,4 +1,4 @@
-const row = document.querySelector('.row')
+const row = document.querySelector('.row');
 row.innerHTML = `<div class="col-md-3">
                     <button id="sort">Sort By</button>
                 </div>
@@ -9,8 +9,40 @@ row.innerHTML = `<div class="col-md-3">
                 <div class="col-md-1"></div>
                 <div class="col-md-3">
                     <button id="add">Add item</button>
-                </div>`
+                </div>`;
 
-const sort = document.getElementById('sort')
-const search = document.getElementById('search')
-const btnAdd = document.getElementById('add')
+const sort = document.getElementById('sort');
+const search = document.getElementById('search');
+const btnAdd = document.getElementById('add');
+
+localStorage.setItem([
+    {
+    id: 1,
+    item: 'TV Stand',
+    createdDate: new Date()
+    }/*,
+    {
+    id: 2,
+    item: 'Phone',
+    createdDate: new Date()
+    },
+    {
+    id: 3,
+    item: 'Bread',
+    createdDate: new Date()
+    },
+    {
+    id: 4,
+    item: 'Coffee',
+    createdDate: new Date()
+    },
+    {
+    id: 5,
+    item: 'Bricks',
+    createdDate: new Date()
+    }*/
+   ]
+   )
+const tv = localStorage.getItem('TV Stand')
+
+let todoList = []
