ebb838437e98811adbf728575ded7b74c5cb3e7a
GarlicBuns19 <planterx26@gmail.com>
2022-06-20 14:20:19 +0200

    btn

--- todoApp.js
+++ todoApp.js
@@ -1,12 +1,16 @@
-let row = document.querySelector('.row')
+const row = document.querySelector('.row')
 row.innerHTML = `<div class="col-md-3">
-                <button id="sort">Sort By</button>
+                    <button id="sort">Sort By</button>
                 </div>
                 <div class="col-md-1"></div>
                 <div class="col-md-3">
-                <input type="text" id="search" placeholder="Enter your item">
+                    <input type="text" id="search" placeholder="Enter your item">
                 </div>
                 <div class="col-md-1"></div>
                 <div class="col-md-3">
-                <button id="add">Add item</button>
+                    <button id="add">Add item</button>
                 </div>`
+
+const sort = document.getElementById('sort')
+const search = document.getElementById('search')
+const btnAdd = document.getElementById('add')
