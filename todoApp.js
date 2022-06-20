3d3610feaf2ae81e64927c96a649eaa62e503994
GarlicBuns19 <planterx26@gmail.com>
2022-06-20 14:17:13 +0200

    item

---
+++ todoApp.js
@@ -0,0 +1,12 @@
+let row = document.querySelector('.row')
+row.innerHTML = `<div class="col-md-3">
+                <button id="sort">Sort By</button>
+                </div>
+                <div class="col-md-1"></div>
+                <div class="col-md-3">
+                <input type="text" id="search" placeholder="Enter your item">
+                </div>
+                <div class="col-md-1"></div>
+                <div class="col-md-3">
+                <button id="add">Add item</button>
+                </div>`
