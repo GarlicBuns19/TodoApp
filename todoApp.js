2520132941967ca9070c661128b4e0efb9c7e685
GarlicBuns19 <planterx26@gmail.com>
2022-06-24 09:54:30 +0200

    help

--- todoApp.js
+++ todoApp.js
@@ -1,18 +1,18 @@
 const row = document.querySelector('.row');
 const input = document.getElementById('itemToDo');
 const btnAddItem = document.getElementById('addItem')
-let todoArr = []
+let todoArr = [];
 
-todoLayout = () => {
-    `<div class="col-md-4"></div>
-        <div class="col-md-4">
-            <form>
-                <input type="text" id="itemToDo">
-                <button id="addItem"></button>
-            </form>
-        </div>
-    <div class="col-md-4"></div>`
-}
+(todoLayout = () => {
+    row.innerHTML = `<div class="col-md-4"></div>
+                        <div class="col-md-4">
+                            <form>
+                                <input type="text" id="itemToDo">
+                                <button id="addItem">Add Item</button>
+                            </form>
+                        </div>
+                    <div class="col-md-4"></div>`
+})();
 
 // btnAddItem.addEventListener('click', () => {
 
