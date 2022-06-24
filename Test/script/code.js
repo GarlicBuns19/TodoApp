415c88dd58d96737de90c2809b178b99e0846003
GarlicBuns19 <planterx26@gmail.com>
2022-06-24 21:01:15 +0100

    new 1

--- Test/script/code.js
+++ Test/script/code.js
@@ -9,22 +9,57 @@ JSON.parse(localStorage.getItem('records')) : [
     }
     
 ];
+
 // Add to arrry
 const btnAdd = document.getElementById('btnAdd');
 
-items.forEach((item,index) => {
+btnAdd.addEventListener('click',() => {
+items.push({
+            id: items.length + 1,
+            item: document.getElementById('input').value,
+            createdDate: new Date()
+            })
+    showData();
+    localStorage.setItem('records',JSON.stringify(items));
+});
+
+// The Todo list when you add
+(function showData() {
+    document.getElementById('list').innerHTML = '';
+    items.forEach((item,index) => {
     document.getElementById('list').innerHTML += `
-                                <li>
-                                    <input type="checkbox" id="check">${item.item}
-                                    <i class="bi bi-x" id="remove"></i>
-                                </li>`
-})
-// btnAdd.addEventListener('click',() => {
-//     items.push(
-//         {
-//             id: items.length +1,
-//             item: document.getElementById('input').value,
-//             createdDate: new Data()
-//         }
-//     )
-// })
+        <li id="${index}">
+            <input type="checkbox" id="check"><span>${item.item}</span></input>
+            <i class="bi bi-trash" onclick="deleteItem(${index})"></i>
+        </li>
+    `
+});
+console.table(items);
+})();
+
+// Sort button
+document.querySelector("#btnSort").addEventListener("click", () => {
+    items.sort((a, b) => {
+    let fa = a.item.toLowerCase(),
+        fb = b.item.toLowerCase();
+  
+      if (fa < fb) {
+        return -1;
+      }
+      if (fa > fb) {
+        return 1;
+      }
+      return 0;
+    });
+    loadData();
+  });
+
+// Delete
+function deleteItem(id) {
+    if (id > -1) {
+      items.splice(id, 1);
+      // Apply the change
+      localStorage.setItem('records', JSON.stringify(items));
+    }
+    loadData();
+  }
